e5dd50c372cabc5201acf4797ce62ae436cca7a8
Zane Xu <wo1996zhi@vip.qq.com>
2016-10-17 22:32:23 +0800

    sample增加存储空间访问的权限请求

--- sample/src/main/java/com/example/zane/sample/MainActivity.java
+++ sample/src/main/java/com/example/zane/sample/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.zane.sample;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.zane.easyimageprovider.builder.ImageProviderBuilder;
 import com.example.zane.easyimageprovider.builder.core.EasyImage;
@@ -26,10 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
     private EasyImage easyImage;
 
+    private static final int PERMISSION_CODE = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if (BuildConfig.VERSION_CODE >= 23){
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission_group.STORAGE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+            }
+        }
 
         buttonCamera = (Button)findViewById(R.id.button_camera);
         buttonAlbum = (Button)findViewById(R.id.button_album);
@@ -66,6 +84,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "拒绝授权", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
     /**
